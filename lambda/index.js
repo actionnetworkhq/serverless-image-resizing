cc46130e0f46425ea290ec07506a9615c1e76a3e
Brian Dittmer <brian.t.dittmer@gmail.com>
2017-12-19 15:31:50 -0800

    preserve the file type

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,7 @@
 
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
-  signatureVersion: 'v4',
+    signatureVersion: 'v4'
 });
 const Sharp = require('sharp');
 
@@ -10,30 +10,37 @@ const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
+    const key = event.queryStringParameters.key;
+    const match = key.match(/\/(\d+)\/(\d+)\/(\d+)x(\d+)\/(.*)/);
+    const width = parseInt(match[1], 10);
+    const height = parseInt(match[2], 10);
+    const originalKey = match[3];
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
-    .catch(err => callback(err))
-}
+    const fileParts = originalKey.split('.');
+    const type = fileParts[fileParths.length - 1];
+
+    S3.getObject({ Bucket: BUCKET, Key: originalKey })
+        .promise()
+        .then(data =>
+            Sharp(data.Body)
+                .resize(width, height)
+                .toFormat(type)
+                .toBuffer()
+        )
+        .then(buffer =>
+            S3.putObject({
+                Body: buffer,
+                Bucket: BUCKET,
+                ContentType: `image/${type}`,
+                Key: key
+            }).promise()
+        )
+        .then(() =>
+            callback(null, {
+                statusCode: '301',
+                headers: { location: `${URL}/${key}` },
+                body: ''
+            })
+        )
+        .catch(err => callback(err));
+};
